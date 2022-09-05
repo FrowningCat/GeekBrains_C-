99fd12613c4e95f38185d4052be2bd45c2ec6c3a
FrowningCat <vsavinkov60@gmail.com>
2022-09-05 17:59:41 +0300

    add Seminar6

---
+++ Seminar6/Task_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+            Console.Write("Введите длину массива");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            if(a <= 0)
+            {
+                Console.WriteLine("Введено неверное число");
+            }
+
+            int[] arrey = new int[a];
+
+            void CreatArrey(int a) // Заполняем массив
+            {
+                for (int i = 0; i < a; i++)
+                {
+                    Console.Write($"Введи {i+1} число: ");
+                    arrey[i] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+
+            int Consider(int[] arrey) // Узнаём сколько чисел > 0
+            {
+                int count = 0;
+                for (int i = 0; i < arrey.Length; i++)
+                {
+                    if(arrey[i] > 0 ) count += 1; 
+                }
+                return count;
+            }
+
+            CreatArrey(a);
+
+            Console.WriteLine($"Чисел больше 0: {Consider(arrey)} ");
+        }
+    }
+}
---
+++ Seminar6/Task_2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Напишите программу, которая найдёт точку пересечения двух прямых, 
+            // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+            // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+            Console.Write("Введите координату k1 ");
+            double k1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координату k2 ");
+            double k2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координату b1 ");
+            double b1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите координату b1 ");
+            double b2 = Convert.ToDouble(Console.ReadLine());
+            
+            double x;
+            double y;
+
+            if ((k1 == k2) && (b1 == b2))
+            {
+                Console.Write("Прямые совпадают");
+            }
+
+            else if (k1 == k2)
+            {
+                Console.Write("Прямые параллельны");
+            }
+
+            else
+            {
+                x=(b2-b1)/(k1-k2);
+                y=(k1*(b2-b1))/(k1-k2)+b1;
+                Console.Write($"({x};{y})");
+            }
+        }
+    }
+}
